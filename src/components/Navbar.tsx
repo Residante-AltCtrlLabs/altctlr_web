f04c86f731365b04ca1399e267fd8b332ad29969
hamzaanis10 <hamzaanis2014@gmail.com>
2025-09-12 22:19:07 +0500

    navbar fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,8 @@ const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="top-0 left-0 right-0 z-50">
+      <div className="max-w-7xl mx-auto px-4 pt-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
