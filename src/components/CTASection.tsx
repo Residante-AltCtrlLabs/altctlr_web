ae80e8acd48b8960deeef4116b3512af314f3793
hamzaanis10 <hamzaanis2014@gmail.com>
2025-09-25 21:31:55 +0500

    pushing for merging

--- src/components/CTASection.tsx
+++ src/components/CTASection.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 
 const CTASection = () => {
+  console.log("Pushing");
   return (
     <section className="w-full bg-white !py-44 md:py-24">
       <div className="max-w-4xl mx-auto px-6 md:px-10 lg:px-12 text-center">
